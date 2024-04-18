76837457f90a0167bc648e5b928dd0e9e2331644
Fireshield546 <soniccoolguy4@gmail.com>
2024-04-18 15:16:55 -0400

    map and simple movement

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        controller.Move(move * Time.deltaTime * speed);
+    }
+}
